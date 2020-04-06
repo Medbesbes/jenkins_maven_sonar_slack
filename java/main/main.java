ae27ee3351d1b0b0537102fdbc4fa22f1eb59b31
Mohamed BESBES <med.besbes@yahoo.fr>
2020-04-06 23:46:00 +0200

    initial commit

---
+++ java/main/main.java
@@ -0,0 +1,31 @@
+package main;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import Services.AgriculteurServiceRemote;
+import entities.Agriculteur;
+import entities.AgriculteurType;
+import entities.Terrain;
+import entities.TerrainType;
+//import entities.;
+import entities.Utilisateur;
+
+
+
+public class main {
+
+	public static void main(String[] args) throws NamingException {
+		
+	
+		
+         System.out.println("Hello World Mohamed BESBES");	
+		
+
+		
+
+	}
+
+}
